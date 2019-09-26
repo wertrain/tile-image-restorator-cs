7cfe85169db4dbc0b360251fcfad5ef105960cf6
wertrain <wertrain@gmail.com>
2019-09-27 00:50:13 +0900

    判定部分の抽象化用のクラスを追加

---
+++ TileImageRestoratorCLI/TileImageRestoratorCLI/ImageComparable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace ImageComparison
+{
+    public abstract class ImageComparisonContext
+    {
+        public abstract IImageComparable FromImage(Image image);
+    }
+
+    public interface IImageComparable
+    {
+        double GetDistanceFrom(IImageComparable imageComparable);
+    }
+}
